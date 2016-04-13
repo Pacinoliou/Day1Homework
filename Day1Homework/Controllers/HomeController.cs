ed9a7af1e95a4979bb33b358f286238f95a7836b
Pacinoliou <assemblykiller@so-net.net.tw>
2016-04-13 22:58:48 +0800

    New

---
+++ Day1Homework/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Day1Homework.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Day1Homework.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult GridChildView( int objSeqNo)
+        {
+
+            var objGridViewModels = new GridViewModels { SeqNo = objSeqNo,
+                                                         AccType ="支出",
+                                                         RecDate = DateTime.Now.AddDays(objSeqNo - 1).ToString("yyyy/MM/dd"),
+                                                         SetMoney = 500 * objSeqNo };
+
+            return View(objGridViewModels);
+
+        }
+      
+    }
+}
---
+++ Day1Homework/Models/GridViewModels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1Homework.Models
+{
+    public class GridViewModels
+    {
+        public int SeqNo { get; set; }
+
+        public string AccType { get; set; }
+
+        public string RecDate { get; set; }
+
+        public int SetMoney { get; set; }
+
+
+    }
+}
